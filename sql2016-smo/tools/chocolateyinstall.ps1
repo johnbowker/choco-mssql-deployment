59fced8bb6b838f9daea8aea91248118c5e19fdb
adm-john-bowker <john.bowker@xero.com>
2017-07-31 11:47:41 +1200

    Added sql2016 smo package.

---
+++ sql2016-smo/tools/chocolateyinstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName= $env:ChocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.microsoft.com/download/8/7/2/872BCECA-C849-4B40-8EBE-21D48CDF1456/ENU/x86/SharedManagementObjects.msi'
+$url64      = 'https://download.microsoft.com/download/8/7/2/872BCECA-C849-4B40-8EBE-21D48CDF1456/ENU/x64/SharedManagementObjects.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = "Microsoft SQL Server 2016 Management Objects (x$($arch))"
+
+  checksum      = '59e80344fdc248ff20a3a141802897aafd58cd84917de686d1b92b930350f2ca'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = 'ea38d5231f6678089316d744edc5b074af735ddd933fd665ce961f602616640c'
+  checksumType64= 'sha256' #default is checksumType
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
